6a13ed532b7f8fde4b50a068232cce9a152a2a67
Hiếu Nghĩa <120780833+SeutaBlue@users.noreply.github.com>
2024-12-30 14:49:45 +0700

    update mot ngay met moi

--- user-role/src/pages/Booking.js
+++ user-role/src/pages/Booking.js
@@ -3,6 +3,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import '../styles/Booking.css';
 
 function Booking() {
+
+    const [step, setStep] = useState(1);
+
     return (
         <div className='booking_section'>
             <div className='display_frame'>
@@ -26,57 +29,64 @@ function Booking() {
                         <span>Thanh toán</span>
                     </div>
                 </div>
-                <div className='booking_step1_frame'>
-                    <div className='booking_info booking_location_info'>
-                        <div className='booking_location_top'>
-                            <div className='booking_location_top_left'>
-                                <img src='/images/detail/detail2.jpg'/>
-                            </div>
-                            <div className='booking_location_top_right'>
-                                <span className='booking_title'>Ho Coc, Vung Tau</span>
-                                <div className='booking_location_price'>VNĐ 5.000.000</div>
-                            </div>
-                        </div>
-                        <div className='booking_location_bottom'>
-                            {/* <div className='booking_location_top_left'>
+                {step === 1 &&
+                    <div className='booking_step1_frame'>
+                        <div className='booking_info_frame'>
+                            <div className='booking_info booking_location_info'>
+                                <div className='booking_location_top'>
+                                    <div className='booking_location_top_left'>
+                                        <img src='/images/detail/detail2.jpg' />
+                                    </div>
+                                    <div className='booking_location_top_right'>
+                                        <span className='booking_title'>Ho Coc, Vung Tau</span>
+                                        <div className='booking_location_price'>VNĐ 5.000.000</div>
+                                    </div>
+                                </div>
+                                <div className='booking_location_bottom'>
+                                    {/* <div className='booking_location_top_left'>
                                 <img src='/images/detail/detail2.jpg'/>
                             </div>
                             <div className='booking_location_top_right'>
                                 <span className='booking_location_name'>Ho Coc, Vung Tau</span>
                                 <div className='booking_location_price'>VNĐ 5.000.000</div>
                             </div> */}
-                        </div>
-                    </div>
-                    <div className='booking_info booking_detail_info'>
-                        {/* <div className='booking_detail_top'> */}
-                            <span className='booking_title booking_detail_top_title'>Chi tiết phòng của bạn</span>
-                            <div className='booking_detail_time'>
-                                <span className='type_date'>Nhận phòng: </span>
-                                <span className='date_value'>T7, 17 tháng 10 năm 2024</span>
+                                </div>
                             </div>
-                            <div className='booking_detail_time'>
-                                <span className='type_date'>Trã phòng: </span>
-                                <span className='date_value'>CN, 18 tháng 10 năm 2024</span>
-                            </div>
-                            <div className='booking_detail'>
-                                <span className='booking_title'>Tổng thời gian:</span>
-                                <span className='booking_value'>1 đêm</span>
-                            </div>
-                            <div className='booking_detail'>
-                                <span className='booking_title'>Số lượng phòng:</span>
-                                <span className='booking_value'>1</span>
-                            </div>
-                            <div className='booking_total_price'>
-                                <span className='booking_total_title'>Tổng cộng</span>
-                                <span className='booking_total_value'>VNĐ 500.000</span>
-                            </div>
-                            {/* <span className='booking_title'>yeu</span> */}
-                        {/* </div>
+                            <div className='booking_info booking_detail_info'>
+                                {/* <div className='booking_detail_top'> */}
+                                <span className='booking_title booking_detail_top_title'>Chi tiết phòng của bạn</span>
+                                <div className='booking_detail_time'>
+                                    <span className='type_date'>Nhận phòng: </span>
+                                    <span className='date_value'>T7, 17 tháng 10 năm 2024</span>
+                                </div>
+                                <div className='booking_detail_time'>
+                                    <span className='type_date'>Trã phòng: </span>
+                                    <span className='date_value'>CN, 18 tháng 10 năm 2024</span>
+                                </div>
+                                <div className='booking_detail'>
+                                    <span className='booking_title'>Tổng thời gian:</span>
+                                    <span className='booking_value'>1 đêm</span>
+                                </div>
+                                <div className='booking_detail'>
+                                    <span className='booking_title'>Số lượng phòng:</span>
+                                    <span className='booking_value'>1</span>
+                                </div>
+                                <div className='booking_total_price'>
+                                    <span className='booking_total_title'>Tổng cộng</span>
+                                    <span className='booking_total_value'>VNĐ 500.000</span>
+                                </div>
+                                {/* <span className='booking_title'>yeu</span> */}
+                                {/* </div>
                         <div className='booking_detail_bottom'>
 
                         </div> */}
+                            </div>
+                        </div>
+                        <div className='booking_btn_frame'>
+                            <button>Tiếp theo</button>
+                        </div>
                     </div>
-                </div>
+                }
             </div>
             {/* <i class="fa-solid fa-address-book"></i>
             <i class="fa-regular fa-id-card"></i> */}
